ba3eda5088ffebad869f698ba021bffe73dec855
SHIRO <shiropasik@gmail.com>
2019-03-28 15:48:15 +0400

    File and Database 2

---
+++ InternshipTest/Person/IStudentIO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InternshipTest.Person;
+
+namespace InternshipTest.Person
+{
+    //Інтерфейс вводу виводу даних класу Student
+    public interface IStudentIO
+    {
+        //Метод інтерфейсу який зберігає дані
+        bool Save(List<Student> students, object config);
+
+        //Метод інтерфейсу який загружає дані в програму
+        List<Student> Open(object config);
+    }
+}
